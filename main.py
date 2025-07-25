dc0de7fc1730c19f5105c02d68a5a4afe063ee60
dillnaza <99315866+dillnaza@users.noreply.github.com>
2025-07-25 11:57:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,127 +1,123 @@
 import telebot
+from datetime import datetime
 
-bot = telebot.TeleBot('token')
+TOKEN = 'your_token_here'
+bot = telebot.TeleBot(TOKEN)
 
+current_year = datetime.now().year
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    name = f'Hi, <b>{message.from_user.first_name}</b>'
+    name = f'Hi, <b>{message.from_user.first_name}</b>!'
     bot.send_message(message.chat.id, name, parse_mode='html')
 
 
 @bot.message_handler(commands=['help'])
-def start(message):
+def help_handler(message):
     bot.send_message(message.chat.id, """
-<b>/start</b> -> start bot
-<b>/help</b> -> show all commands
-<b>/find_age</b> -> enter year that you born to calculate your age
-<b>/find_animal</b> -> enter year that you born to know your animal-symbol from "Eastern Animal Calendar" 
-<b>/find_zodiac</b> -> enter day and month that you born and know who you are by zodiac sign
+<b>/start</b> - Start the bot  
+<b>/help</b> - Show all commands  
+<b>/find_age</b> - Calculate your age  
+<b>/find_animal</b> - Find your Eastern calendar animal  
+<b>/find_zodiac</b> - Find your Zodiac sign
 """, parse_mode='html')
 
 
-year = 2003
-date = 23
-month = 5
-
-
 @bot.message_handler(commands=['find_age'])
-def find_age(message):
-    bot.send_message(message.chat.id, "Enter year: ")
-    bot.register_next_step_handler(message, get_year_for_age)
+def ask_birth_year(message):
+    bot.send_message(message.chat.id, "Enter your birth year:")
+    bot.register_next_step_handler(message, calculate_age)
+
+
+def calculate_age(message):
+    try:
+        birth_year = int(message.text)
+        age = current_year - birth_year
+        if 0 < age < 150:
+            bot.send_message(message.chat.id, f'Your age is <b>{age}</b>', parse_mode='html')
+        else:
+            bot.send_message(message.chat.id, "Are you a vampire? Please enter a realistic year.")
+    except ValueError:
+        bot.send_message(message.chat.id, "Please enter a valid number.")
 
 
 @bot.message_handler(commands=['find_animal'])
-def find_animal(message):
-    bot.send_message(message.chat.id, "Enter year: ")
-    bot.register_next_step_handler(message, get_year)
+def ask_animal_year(message):
+    bot.send_message(message.chat.id, "Enter your birth year:")
+    bot.register_next_step_handler(message, calculate_animal)
 
 
-def get_year_for_age(message):
-    global year
-    year = int(message.text)
-    bot.send_message(message.chat.id, f'Your age: {2022 - year}', parse_mode='html')
-
-
-def get_year(message):
-    global year
-    s = ''
-    year = int(message.text)
-    y = 11 - abs(2000 - year) % 11
-    match y:
-        case 11:
-            s = 'Sheep'
-        case 10:
-            s = 'Horse'
-        case 9:
-            s = 'Snake'
-        case 8:
-            s = 'Dragon'
-        case 7:
-            s = 'Rabbit'
-        case 6:
-            s = 'Tiger'
-        case 5:
-            s = 'Bull'
-        case 4:
-            s = 'Rat'
-        case 3:
-            s = 'Wild boar'
-        case 2:
-            s = 'Dog'
-        case 1:
-            s = 'Rooster'
-        case 0:
-            s = 'Monkey'
-
-    bot.send_message(message.chat.id,
-                     f'{year} years`s animal-symbol from "Eastern Animal Calendar" is <b><i>{s}</i></b>',
-                     parse_mode='html')
+def calculate_animal(message):
+    try:
+        year = int(message.text)
+        animals = [
+            'Monkey', 'Rooster', 'Dog', 'Wild boar', 'Rat', 'Bull',
+            'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep'
+        ]
+        index = (year - 2000) % 12
+        symbol = animals[index]
+        bot.send_message(message.chat.id,
+                         f'Your Eastern calendar animal for {year} is <b><i>{symbol}</i></b>',
+                         parse_mode='html')
+    except ValueError:
+        bot.send_message(message.chat.id, "Please enter a valid year.")
 
 
 @bot.message_handler(commands=['find_zodiac'])
-def find_zodiac(message):
-    s = ''
-    bot.send_message(message.chat.id, "Enter your birthday: ")
-    bot.register_next_step_handler(message, get_day)
+def ask_zodiac_day(message):
+    bot.send_message(message.chat.id, "Enter your birth day (1-31):")
+    bot.register_next_step_handler(message, get_zodiac_day)
 
 
-def get_day(message):
-    global date
-    date = int(message.text)
-    bot.send_message(message.chat.id, "Enter the month of birth: ")
-    bot.register_next_step_handler(message, get_month)
+def get_zodiac_day(message):
+    try:
+        day = int(message.text)
+        if 1 <= day <= 31:
+            message.chat_data = {'day': day}
+            bot.send_message(message.chat.id, "Enter your birth month (1-12):")
+            bot.register_next_step_handler(message, get_zodiac_month, day)
+        else:
+            bot.send_message(message.chat.id, "Please enter a valid day between 1 and 31.")
+    except ValueError:
+        bot.send_message(message.chat.id, "Please enter a valid number.")
 
 
-def get_month(message):
-    global month
-    s = ''
-    month = int(message.text)
-    if (21 <= date <= 31 and month == 3) or (month == 4 and 1 <= date <= 19):
-        s = 'Aries'
-    elif (20 <= date <= 30 and month == 4) or (month == 5 and 1 <= date <= 20):
-        s = 'Taurus'
-    elif (21 <= date <= 31 and month == 5) or (month == 6 and 1 <= date <= 21):
-        s = 'Twins'
-    elif (22 <= date <= 30 and month == 6) or (month == 7 and 1 <= date <= 22):
-        s = 'Cancer'
-    elif (23 <= date <= 31 and month == 7) or (month == 8 and 1 <= date <= 22):
-        s = 'Leo'
-    elif (23 <= date <= 31 and month == 8) or (month == 9 and 1 <= date <= 22):
-        s = 'Maid'
-    elif (23 <= date <= 30 and month == 9) or (month == 10 and 1 <= date <= 23):
-        s = 'Scales'
-    elif (24 <= date <= 31 and month == 10) or (month == 11 and 1 <= date <= 22):
-        s = 'Scorpio'
-    elif (23 <= date <= 30 and month == 11) or (month == 12 and 1 <= date <= 21):
-        s = 'Sagittarius'
-    elif (22 <= date <= 31 and month == 12) or (month == 1 and 1 <= date <= 20):
-        s = 'Capricorn'
-    elif (21 <= date <= 31 and month == 1) or (month == 2 and 1 <= date <= 18):
-        s = 'Aquarius'
-    elif (19 <= date <= 29 and month == 2) or (month == 3 and 1 <= date <= 20):
-        s = 'Fishes'
-    bot.send_message(message.chat.id, f'Your zodiac sign is <b><i>{s}</i></b>', parse_mode='html')
+def get_zodiac_month(message, day):
+    try:
+        month = int(message.text)
+        if not (1 <= month <= 12):
+            raise ValueError
+
+        zodiac_sign = get_zodiac_by_date(day, month)
+        if zodiac_sign:
+            bot.send_message(message.chat.id,
+                             f'Your Zodiac sign is <b><i>{zodiac_sign}</i></b>',
+                             parse_mode='html')
+        else:
+            bot.send_message(message.chat.id, "Invalid date. Try again.")
+    except ValueError:
+        bot.send_message(message.chat.id, "Please enter a valid month (1-12).")
+
+
+def get_zodiac_by_date(day, month):
+    zodiac_dates = [
+        ((3, 21), (4, 19), 'Aries'),
+        ((4, 20), (5, 20), 'Taurus'),
+        ((5, 21), (6, 20), 'Gemini'),
+        ((6, 21), (7, 22), 'Cancer'),
+        ((7, 23), (8, 22), 'Leo'),
+        ((8, 23), (9, 22), 'Virgo'),
+        ((9, 23), (10, 22), 'Libra'),
+        ((10, 23), (11, 21), 'Scorpio'),
+        ((11, 22), (12, 21), 'Sagittarius'),
+        ((12, 22), (1, 19), 'Capricorn'),
+        ((1, 20), (2, 18), 'Aquarius'),
+        ((2, 19), (3, 20), 'Pisces')
+    ]
+    for start, end, sign in zodiac_dates:
+        if (month == start[0] and day >= start[1]) or (month == end[0] and day <= end[1]):
+            return sign
+    return None
 
 
 bot.polling(none_stop=True)
